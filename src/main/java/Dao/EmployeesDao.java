4083685284498faf54e303fb328f6221b0dc47e6
Hammertonmutuku <lthamma3@gmail.com>
2020-01-20 10:55:19 +0300

    added a constructor at news.java

--- src/main/java/Dao/EmployeesDao.java
+++ src/main/java/Dao/EmployeesDao.java
@@ -1,4 +1,22 @@
 package Dao;
 
+import models.Employees;
+
+import java.util.List;
+
 public interface EmployeesDao {
+    //create
+    void add(Employees employees);
+
+    //read
+    List<Employees> getAll();
+
+
+    //update
+    void update(String employees_name, String position_in_company, int employee_id, String role, String department );
+
+    //delete
+    void deleteByEmployee_id(int employee_id);
+    void clearAll();
 }
+
