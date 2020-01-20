f9c83e8b0b1f6854b5c2a898821503d0ba20a2c4
Hammertonmutuku <lthamma3@gmail.com>
2020-01-20 10:10:10 +0300

    added data to employees interface

--- src/main/java/Dao/DepartmentsDao.java
+++ src/main/java/Dao/DepartmentsDao.java
@@ -1,4 +1,20 @@
 package Dao;
 
+import models.Departments;
+import java.util.List;
+
 public interface DepartmentsDao {
+    //create
+    void add(Departments departments);
+
+    //read
+    List<Departments> getAll();
+
+
+    //update
+    void update(String department_name, String department_description, int number_of_employees);
+
+    //delete
+    void deleteByDepartment_id(int department_id);
+    void clearAll();
 }
--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -5,11 +5,13 @@ public class Departments {
     private String department_name;
     private String department_description;
     private int number_of_employees;
+    private int department_id;
     /*Department name, description, number of employees, */
 
-    public Departments(String department_name, String department_description, int number_of_employees) {
+    public Departments(String department_name, String department_description, int number_of_employees,int department_id) {
         this.department_name = department_name;
         this.department_description = department_description;
         this.number_of_employees = number_of_employees;
+        this.department_id = department_id;
     }
 }
