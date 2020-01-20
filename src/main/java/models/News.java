6ae08b0dd0eee460e53883e1fb928d44959cde98
Hammertonmutuku <lthamma3@gmail.com>
2020-01-20 09:46:18 +0300

    created my database

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -2,4 +2,11 @@ package models;
 
 public class News {
     /* name, position in the company, their roles;  */
+    private String general_news;
+    private String department_news;
+
+    public News(String general_news, String department_news) {
+        this.general_news = general_news;
+        this.department_news = department_news;
+    }
 }
