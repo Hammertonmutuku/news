108efe2da2f4b23c245ac0d39e6b0083c5f9af0e
Hammertonmutuku <lthamma3@gmail.com>
2020-01-21 14:46:56 +0300

    added on imports @app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentsDao;
+import dao.sql2oEmployeesDao;
+import dao.sql2oNewsDao;
+import models.Departments;
+import models.News;
+import models.Employees;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
 public class App {
+
 }
